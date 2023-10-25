3f96f9fa903ade1d5c26b962f4f13da4770f768e
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-10-25 21:51:29 +0530

    Update Static-Page.js

--- src/Static-Page.js
+++ src/Static-Page.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo192.png'; 
 
-const data = (
-  <div>
+
+function Header(){
+  return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="React Logo" width="92" height="92" />
       <h1 style={{ marginLeft: "20px" }}>Fun facts about React</h1>
     </div>
+  )
+}
+function MainContent(){
+  return (
     <ul style={{ textAlign: "left" }}>
       <li>Was first released in 2013</li>
       <li>Was originally created by JORDAN WALKE</li>
@@ -14,7 +19,16 @@ const data = (
       <li>is maintained by Facebook</li>
       <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
+  )
+}
+
+
+const data = (
+  <div>
+      <Header />
+      <MainContent />
   </div>
 );
+
 
 export default data;
