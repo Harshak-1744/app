c6e7a72f233076496e2ad96cf5010d4781ed3cdc
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-10-26 23:22:09 +0530

    Update Static-Page.js

--- src/Static-Page.js
+++ src/Static-Page.js
@@ -1,49 +1,52 @@
 import React from 'react';
-import logo from './logo192.png'; 
+import logo from './logo192.png';
 
-
-function Header(){
+function Header() {
   return (
     <header>
       <nav className="nav">
         <img className="logo" src={logo} width="40px" />
-          <ul className="nav-items">
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
-  </header>
-  )
+    </header>
+  );
 }
 
-function MainContent(){
+function MainContent() {
   return (
     <div>
       <h1 className="main">Reasons I'm excited to learn React</h1>
       <ol>
-          <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-          <li>I'm more likely to get a job as a developer if I know React</li>  
+        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+        <li>
+          React has powerful features, such as virtual DOM, reusable components, and state management,
+          make it easier to build scalable and maintainable applications.
+        </li>
       </ol>
     </div>
-  )
+  );
 }
+
 
 function Footer() {
   return (
-      <footer className="footer">
-          <small>© 2023 HK development. All rights reserved.</small>
-      </footer>
-  )
+    <footer className="footer">
+      <small>© 2023 HK development. All rights reserved.</small>
+    </footer>
+  );
 }
 
 const data = (
   <div>
-      <Header />
-      <MainContent />
-      <Footer />
+    <Header />
+    <MainContent />
+    <Footer />
   </div>
 );
-
 
 export default data;
