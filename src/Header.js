b2f486066814b1cbbabcef443205a964b31bd36e
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-10-31 13:16:20 +0530

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,6 @@ function Header() {
         <nav className="nav">
           <img className="logo" src={logo} alt ="this is the logo"width="40px" />
           <ul className="nav-items">
-            <li>Pricing</li>
             <li>About</li>
             <li>Contact</li>
           </ul>
--- src/MainContent.js
+++ src/MainContent.js
@@ -4,14 +4,14 @@ function MainContent() {
     return (
       <div>
         <h1 className="main">Reasons I'm excited to learn React</h1>
-        <ol>
+        <ul className='main--ul'>
           <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
           <li>I'm more likely to get a job as a developer if I know React</li>
           <li>
             React has powerful features, such as virtual DOM, reusable components, and state management,
             make it easier to build scalable and maintainable applications.
           </li>
-        </ol>
+        </ul>
       </div>
     );
   }
