1cf642b3c653338313f24fbbca3fd0bb64f164d9
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-11-01 13:39:54 +0530

    Add files via upload

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Bounce from 'react-reveal/Bounce';
 
 function MainContent() {
     return (
       <div>
-        <h1 className="main">Reasons I'm excited to learn React</h1>
+        <h1 className="main">
+          <Bounce bottom>Reasons I'm excited to learn React</Bounce>
+        </h1>
         <ul className='main--ul'>
           <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
           <li>I'm more likely to get a job as a developer if I know React</li>
           <li>
             React has powerful features, such as virtual DOM, reusable components, and state management,
-            make it easier to build scalable and maintainable applications.
+            making it easier to build scalable and maintainable applications.
           </li>
         </ul>
       </div>
     );
-  }
+}
 
-export default MainContent
+export default MainContent;
